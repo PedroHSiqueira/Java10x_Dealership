ce67f026b1ce7d0c03ccc44f79c87785b3f235e6
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-05-02 19:51:05 -0300

    Feat: Criação das camadas de Service e Repository (Arquitetura)

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Car/service/CarService.java
@@ -0,0 +1,4 @@
+package dev.java10x.CadastroDeNinjas.Car.service;
+
+public class CarService {
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/User/repository/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.java10x.CadastroDeNinjas.User.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NinjaRepository {
+}
